d0b5307fc46c611f0737b06d1bbaa4ae09b5f843
GrigoryCode <grigorycode@gmail.com>
2019-03-24 13:03:26 +0300

    Modern admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Pizza
+from .models import Pizza, Offer
 
 
-admin.site.register(Pizza)
+class PizzaAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'price')
 
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount')
+
+
+admin.site.register(Pizza, PizzaAdmin)
+admin.site.register(Offer, OfferAdmin)
