e93f3ea97b9a1b6a1fbbbbf3b96fef2f1989125d
GrigoryCode <grigorycode@gmail.com>
2019-03-24 12:23:13 +0300

    Remake db, superuser

--- products/models.py
+++ products/models.py
@@ -3,5 +3,12 @@ from django.db import models
 
 class Pizza(models.Model):
     title = models.CharField(max_length=50)
+    description = models.TextField(max_length=300)
     price = models.FloatField()
     image_url = models.CharField(max_length=2083)
+
+
+class Offer(models.Model):
+    code = models.CharField(max_length=10)
+    description = models.TextField(max_length=300)
+    discount = models.FloatField()
