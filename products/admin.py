3b5cbcb3b4bf267f412b9849526dbb89e2e24428
GrigoryCode <grigorycode@gmail.com>
2019-03-24 21:48:43 +0300

    Local images for cards

--- products/admin.py
+++ products/admin.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
-from .models import Pizza, Offer
+from .models import Pizza
 
 
 class PizzaAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'price')
 
 
-class OfferAdmin(admin.ModelAdmin):
-    list_display = ('code', 'discount')
-
-
 admin.site.register(Pizza, PizzaAdmin)
-admin.site.register(Offer, OfferAdmin)
--- products/models.py
+++ products/models.py
@@ -5,10 +5,5 @@ class Pizza(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=300)
     price = models.FloatField()
-    image_url = models.CharField(max_length=2083)
+    image = models.ImageField(upload_to='images')
 
-
-class Offer(models.Model):
-    code = models.CharField(max_length=10)
-    description = models.TextField(max_length=300)
-    discount = models.FloatField()
