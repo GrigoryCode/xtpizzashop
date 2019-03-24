1e2ebef780970e225c3319a15fdc2ab22793307a
GrigoryCode <grigorycode@gmail.com>
2019-03-24 10:39:54 +0300

    First one

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/983a7165-cb3e-48f5-b907-c00b809f7d9b/Django/XtPizza/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
