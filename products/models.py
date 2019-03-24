60ba1bd62ae8b2eefb0a27f1c37c1c66e634b193
GrigoryCode <grigorycode@gmail.com>
2019-03-24 11:42:33 +0300

    Pizza model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Pizza(models.Model):
+    title = models.CharField(max_length=50)
+    price = models.FloatField()
+    image_url = models.CharField()
